cac9d6acb344363fbb99d0814048a576f5d662e0
tomung-puppy <yw3835@gmail.com>
2025-09-24 14:36:57 +0900

    add_prog4_and_directory

---
+++ yongwoo/[PROG]-4-yongwoo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+void list_04_20(void);
+
+int main()
+{
+    list_04_20();
+    return 0;
+}
+
+void list_04_20()
+{
+    using namespace std;
+    char animal[20] = "bear";
+    char* ps;
+    
+    //cout << "동물를 입력하세요. ";
+    //cin >> animal;
+
+    ps = animal;
+    //ps = "bear"; // 문자열 상수는 char*로 변환이 안됨
+    cout <<"ps : " << ps << "s!\n";
+    cout << "* ps : " << *ps << endl;
+    cout << "ps[0] : " << ps[0] << endl;
+    cout << "&ps : " << &ps << endl;
+    cout << "&animal : " << &animal << endl;
+    cout << "&animal[0] : " << &animal[0] << endl;
+    cout << "(int *) ps : " << (int *) ps << endl;
+    cout << "(int *) animal : " << (int*) animal << endl;
+
+
+    cout << "animal : " << animal << endl;
+}
